58746265c476b50a8235b095a8852d1129f72fdc
Espen Myrland <espenm@met.no>
2015-05-15 12:31:12 +0200

    api binary name has changed.
    Summary: api binary name has changed, chenge upstart script accordingly
    
    Test Plan: N/A
    
    Reviewers: #bora-prod, michaeloa
    
    Reviewed By: #bora-prod, michaeloa
    
    Subscribers: #bora-prod
    
    Differential Revision: https://phab.met.no/D608

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,4 +8,4 @@ default['met-api']['deploy_directory'] = '/opt/met-api-1.0-SNAPSHOT'
 # Auto-generated application settings
 default['met-api']['listen_address'] = '0.0.0.0'
 default['met-api']['listen_port'] = 9000
-default['met-api']['server_binary'] = "#{node['met-api']['deploy_directory']}/bin/met-api"
+default['met-api']['server_binary'] = "#{node['met-api']['deploy_directory']}/bin/metapi"
