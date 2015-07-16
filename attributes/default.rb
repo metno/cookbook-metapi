d652325918dadd332d8f3eafe2e668bf1e650b1b
Espen Myrland <espenm@met.no>
2015-07-16 10:57:53 +0200

    force_default java

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 # For met-api application servers
-default['java']['install_flavor'] = 'oracle'
-default['java']['jdk_version'] = '8'
-default['java']['oracle']['accept_oracle_download_terms'] = true
-default['java']['oracle']['jce']['enabled'] = true
+force_default['java']['install_flavor'] = 'oracle'
+force_default['java']['jdk_version'] = '8'
+force_default['java']['oracle']['accept_oracle_download_terms'] = true
+force_default['java']['oracle']['jce']['enabled'] = true
 
 # Application settings
 default['met-api']['deploy_directory'] = '/opt/met-api-1.0-SNAPSHOT'
